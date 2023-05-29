93edb0016ca6eb1dd166e88c43cae06810e57687
Owen <socknot@live.com>
2023-05-29 13:30:35 -0700

    initial setup, installed tailwind, included basic css in tailwind.config & index.css, created initial database in index.js

---
+++ whitecap_site/src/constants/index.js
@@ -0,0 +1,166 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "sample",
+    content:
+      "sample",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "sample",
+    content:
+      "sample",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "sample",
+    content:
+      "sample",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "sample",
+    name: "sample",
+    title: "sample",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "sample",
+    name: "sample",
+    title: "sample",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "sample",
+    name: "sample",
+    title: "sample",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "sample",
+    value: "sample",
+  },
+  {
+    id: "stats-2",
+    title: "sample",
+    value: "sample",
+  },
+  {
+    id: "stats-3",
+    title: "sample",
+    value: "sample",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "",
+      },
+      {
+        name: "How it Works",
+        link: "",
+      },
+      {
+        name: "Create",
+        link: "",
+      },
+      {
+        name: "Explore",
+        link: "",
+      },
+      {
+        name: "Terms & Services",
+        link: "",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Partners",
+        link: "",
+      },
+      {
+        name: "Suggestions",
+        link: "",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "",
+      },
+      {
+        name: "Become a Partner",
+        link: "",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];

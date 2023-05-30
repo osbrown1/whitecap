3573cc57f7ec7a36afd2120d25828bdae374d442
Owen <socknot@live.com>
2023-05-29 17:49:54 -0700

    wip

--- whitecap_site/src/App.jsx
+++ whitecap_site/src/App.jsx
@@ -2,7 +2,7 @@
 
 import styles from './style';
 
-import Navbar from './components/Navbar';
+import { Navbar, Business, Clients, Stats, Footer, Testimonials, Hero } from './components';
 
 
 const App = () => {
@@ -11,24 +11,24 @@ const App = () => {
       
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
       </div>
       </div>
       
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
       </div>
       </div>  
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Testimonials
-          Clients 
-          Footer 
+          <Stats />
+          <Business />
+          <Testimonials />
+          <Clients />
+          <Footer />
       </div>
       </div>  
         
---
+++ whitecap_site/src/components/index.js
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar";
+import Business from "./Business";
+import Clients from "./Clients";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Business,
+  Clients,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
